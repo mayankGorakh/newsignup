df3d34b1d03584882988f11acd05f90b4db6d23c
mayankGorakh <mayankomatic53@gmail.com>
2016-10-07 17:18:04 +0530

    changed some code

---
+++ api/controllers/UserController.js
@@ -0,0 +1,30 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	'new': function (req, res) {
+		res.view();
+	},
+
+	
+	create: function(req, res, next){
+
+		User.create ( req.params.all(), function userCreated  (err, user){
+
+			if(err){
+				console.log(err);
+
+
+				return  res.redirect('user/new');
+			}
+
+			res.json(user);
+		});
+	}
+};
+
