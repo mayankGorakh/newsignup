054dac2402b582b6d7b8eff5bb753a87511c6452
mayankGorakh <mayankomatic53@gmail.com>
2016-10-10 16:33:33 +0530

    bcrypt code added

--- api/models/User.js
+++ api/models/User.js
@@ -40,7 +40,24 @@ module.exports = {
    //delete obj._csrf;
    //return obj;		
 
+  	},
+
+  	beforeCreate: function(values, next){
+
+  		if(values.password || values.password !=values.confirmation){
+
+  			return next({err:["Passwords does not match password confirmation]"});  		}
   	}
+
+  	require('bcrypt').hash(values.password,10,function passwordEncrypted(err,encryptedPassword){
+  		if(err) return next (err);
+  		values.encryptedPassword = encryptedPassword;
+  		//	values.online =true;
+  		next();
+  	});
+  }
+
+
 
 
 };
